99ded0f28494ec5b06f66e84c33a92c905eddac6
Michael Schonfeld <michael@dwolla.com>
2013-10-09 11:34:21 -0400

    simplify things...

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -12,8 +12,6 @@ module Spree
 
       return unless Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id]).kind_of?(Spree::Gateway::Dwolla)
 
-      load_order
-
       payment = @order.payments.create!({
         :source => Spree::DwollaCheckout.create({
           :oauth_token => session[:dwolla_oauth_token],
@@ -23,11 +21,6 @@ module Spree
         :amount => @order.total,
         :payment_method => payment_method
       }, :without_protection => true)
-
-      @order.state = :confirm
-      @order.save
-
-      redirect_to checkout_state_path(@order.state)
     end
 
   end
