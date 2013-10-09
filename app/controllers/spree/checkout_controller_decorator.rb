73470beae88264c21bd7e9224390eee5da0b2009
Michael Schonfeld <michael@dwolla.com>
2013-10-09 12:31:27 -0400

    updateS

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -12,15 +12,15 @@ module Spree
 
       return unless Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id]).kind_of?(Spree::Gateway::Dwolla)
 
-      payment = @order.payments.create!({
+      @order.payments.create!({
         :source => Spree::DwollaCheckout.create({
           :oauth_token => session[:dwolla_oauth_token],
           :pin => params[:dwolla_pin],
           :funding_source_id => params[:dwolla_funding_source] || payment_method.preferred_default_funding_source
-        }, :without_protection => true),
+        }),
         :amount => @order.total,
         :payment_method => payment_method
-      }, :without_protection => true)
+      })
     end
 
   end
