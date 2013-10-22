026dd230eb1613509ee9bfdda76649c0daa4d9f3
Michael Schonfeld <michael@dwolla.com>
2013-10-22 13:05:25 -0400

    add debugging around dwollacontroller's update and bump version
    Conflicts:
    	README.md

--- app/models/spree/gateway/dwolla.rb
+++ app/models/spree/gateway/dwolla.rb
@@ -11,8 +11,9 @@ module Spree
     preference :allow_ach, :boolean, :default => false
     preference :your_oauth_token, :string
     preference :your_pin, :string
+    preference :enable_debug, :boolean, :default => false
 
-    attr_accessible :preferred_dwolla_id, :preferred_key, :preferred_secret, :preferred_oauth_scope, :preferred_sandbox, :preferred_allow_funding_sources, :preferred_default_funding_source, :preferred_allow_ach, :preferred_your_oauth_token, :preferred_your_pin
+    attr_accessible :preferred_dwolla_id, :preferred_key, :preferred_secret, :preferred_oauth_scope, :preferred_sandbox, :preferred_allow_funding_sources, :preferred_default_funding_source, :preferred_allow_ach, :preferred_your_oauth_token, :preferred_your_pin, :preferred_enable_debug
 
     def supports?(source)
       true
