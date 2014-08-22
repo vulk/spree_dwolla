5392dca91140f6b7595ed5cc83f6bbffdde7e6d6
Nima Shariatian <nima@onevio.org>
2014-08-22 16:18:11 -0400

    updates: asset structure for spree 2.3

--- app/assets/javascripts/spree/frontend/spree_dwolla.js
+++ app/assets/javascripts/spree/frontend/spree_dwolla.js
@@ -1,42 +1,41 @@
-//= require store/spree_frontend
-
 SpreeDwolla = {
-  qs: function(key) {
-    key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&");
-    var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"));
-    return match && decodeURIComponent(match[1].replace(/\+/g, " "));
-  },
+    qs: function (key) {
+        key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&");
+        var match = location.search.match(new RegExp("[?&]" + key + "=([^&]+)(&|$)"));
+        return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    },
 
-  hidePaymentSaveAndContinueButton: function(paymentMethod, preauth) {
-    if (SpreeDwolla.paymentMethodID && paymentMethod.val() == SpreeDwolla.paymentMethodID) {
-      if(preauth) { $('.continue').hide(); }
-    } else {
-      if(preauth) { $('.continue').show(); }
+    hidePaymentSaveAndContinueButton: function (paymentMethod, preauth) {
+        if (SpreeDwolla.paymentMethodID && paymentMethod.val() == SpreeDwolla.paymentMethodID) {
+            if (preauth) {
+                $('.continue').hide();
+            }
+        } else {
+            if (preauth) {
+                $('.continue').show();
+            }
+        }
     }
-  }
-}
+};
 
-$(document).ready(function() {
-  var oauthWindow
-    , checkedPaymentMethod = $('div[data-hook="checkout_payment_step"] input[type="radio"]:checked')
-    , paymentMethods = $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function(e) {
-      var preauth = SpreeDwolla.dwollaId ? false : true;
-      SpreeDwolla.hidePaymentSaveAndContinueButton($(e.target), preauth);
-    })
-    ;
+$(document).ready(function () {
+    var oauthWindow,
+        checkedPaymentMethod = $('div[data-hook="checkout_payment_step"] input[type="radio"]:checked'),
+        paymentMethods       = $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function (e) {
+            var preauth = SpreeDwolla.dwollaId ? false : true;
+            SpreeDwolla.hidePaymentSaveAndContinueButton($(e.target), preauth);
+        });
 
-  SpreeDwolla.hidePaymentSaveAndContinueButton(checkedPaymentMethod)
+    SpreeDwolla.hidePaymentSaveAndContinueButton(checkedPaymentMethod)
 
-  if(SpreeDwolla.qs('method') == 'dwolla') {
-    // Switch to the Dwolla payment tab
-  }
+    if (SpreeDwolla.qs('method') == 'dwolla') {
+        // Switch to the Dwolla payment tab
+    }
 
-  $('#dwolla_button').on('click', function(e) {
-    e.preventDefault();
+    $('#dwolla_button').on('click', function (e) {
+        e.preventDefault();
+        oauthWindow = window.open($(this).attr('href'), 'Dwolla OAuth', 'height=400,width=700');
+        return false;
+    });
 
-    oauthWindow = window.open($(this).attr('href'), 'Dwolla OAuth', 'height=400,width=700');
-
-    return false;
-  });
-
-})
+});
