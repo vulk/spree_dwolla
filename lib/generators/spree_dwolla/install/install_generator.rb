c298f5cfae41763bfe2e9d8ca8b15652e20149d6
Nima Shariatian <nima@onevio.org>
2014-08-22 16:19:30 -0400

    updates: generator for spree 2.3

--- lib/generators/spree_dwolla/install/install_generator.rb
+++ lib/generators/spree_dwolla/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeDwolla
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_dwolla\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_dwolla\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_dwolla\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_dwolla\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_dwolla\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_dwolla\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_dwolla\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_dwolla\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
@@ -26,6 +26,7 @@ module SpreeDwolla
           puts 'Skipping rake db:migrate, don\'t forget to run it!'
         end
       end
+
     end
   end
 end
